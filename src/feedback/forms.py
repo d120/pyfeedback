adbb90c6510c7198eef821b1c208db441c472c13
Yadullah Duman <yadullah.duman@gmail.com>
2017-01-14 14:25:30 +0100

    deleted old form for bestellung

--- src/feedback/forms.py
+++ src/feedback/forms.py
@@ -66,65 +66,6 @@ class PersonForm(forms.ModelForm):
             raise forms.ValidationError('Das Feld für die Anrede oder Email ist leer.')
 
 
-class BestellungModelForm(forms.ModelForm):
-    def __init__(self, data=None, *args, **kwargs):
-        super(BestellungModelForm, self).__init__(data, *args, **kwargs)
-
-        if 'instance' not in kwargs:
-            raise KeyError(u'This form needs an instance=... parameter to function properly.')
-
-        veranstalter_queryset = kwargs['instance'].veranstalter.all()
-        self.fields['verantwortlich'].queryset = veranstalter_queryset
-        self.fields['ergebnis_empfaenger'].queryset = veranstalter_queryset
-
-        # Es findet eine Vollerhebung statt unterbinde austragen
-        if kwargs['instance'].semester.vollerhebung:
-            del self.fields['evaluieren']
-
-        # FIXME: Django haengt sonst einen Hilfetext an der nicht zum Widget past
-        self.fields['ergebnis_empfaenger'].help_text = u'An diese Personen werden die Ergebnisse per E-Mail geschickt.'
-
-        # Hänge den Hinweis auf den letzten möglichen Termin an.
-        self.fields['auswertungstermin'].help_text += ' ' + kwargs['instance'].auwertungstermin_to_late_msg()
-
-        # Nutze ein Widget bei dem nur das jahr des letzten Auswertungstermins angegeben werden kann 
-        years_tuple = kwargs['instance'].semester.auswertungstermin_years()
-        self.fields['auswertungstermin'].widget = extras.SelectDateWidget(years=years_tuple)
-
-        # Auswertungstermin kann nur gewählt werden wenn es ein Seminar oder Praktikum ist
-        if kwargs['instance'].typ not in ['se', 'pr']:
-            del self.fields['auswertungstermin']
-
-        # Lösche die Auswahl ob es eine Übung gibt wenn es keine Vorlesung ist
-        vltypes = ['vu', 'v']
-        if kwargs['instance'].typ not in vltypes:
-            del self.fields['typ']
-        else:
-            choices = []
-            for cur in self.fields['typ'].choices:
-                if cur[0] in vltypes:
-                    choices.append(cur)
-
-            self.fields['typ'].choices = choices
-
-            # wenn wir Formular geschickt bekommen
-            # Wenn die Checkbox nicht gesetzt ist sie False
-        if data and (data.get('evaluieren', False) is not False or kwargs['instance'].semester.vollerhebung):
-            # nur wenn Evaluieren angeklickt ist oder Vollerhebung sind die anderen Felder erforderlich
-            for k, field in self.fields.items():
-                field.required = True
-            # min value einfach im Feld zu setzen geht nicht siehe: http://stackoverflow.com/a/3470992
-            self.fields['anzahl'] = forms.IntegerField(min_value=1)
-
-    class Meta:
-        model = Veranstaltung
-        fields = (
-            'evaluieren', 'typ', 'anzahl', 'sprache',
-            'verantwortlich', 'ergebnis_empfaenger', 'auswertungstermin'
-        )
-        widgets = {'ergebnis_empfaenger': forms.CheckboxSelectMultiple}
-
-
 class KommentarModelForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         veranst = kwargs.pop('veranstaltung', None)
