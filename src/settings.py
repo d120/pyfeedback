82bc3b5de6e9fe19497d0e104fced0979cc9e90f
miterion <heiko.carrasco@yahoo.com>
2017-06-30 10:41:21 +0200

    Added missing context processor

--- src/settings.py
+++ src/settings.py
@@ -107,6 +107,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': ['django.contrib.auth.context_processors.auth',
             "django.contrib.messages.context_processors.messages",
+            "django.template.context_processors.request",
             ],
             'loaders': [
             'django.template.loaders.filesystem.Loader',
