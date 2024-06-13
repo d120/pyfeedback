01834d496c6230f9def476d835ffa9fe5663e414
joachimschmidt557 <joachim.schmidt557@outlook.com>
2024-06-13 12:54:39 +0200

    static: add package.json with frontend dependencies

--- src/settings.py
+++ src/settings.py
@@ -71,6 +71,7 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     BASE_PATH + 'media/',
+    BASE_PATH + 'node_modules/',
 )
 
 # List of finder classes that know how to find static files in
