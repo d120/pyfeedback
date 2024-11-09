2332ff5746e5a8e640fe966a6506aea9cb3d6a09
4-dash <120916864+4-dash@users.noreply.github.com>
2024-11-09 18:16:12 +0100

    translate_url templatetag is added, changes were made to make code compatible with it

---
+++ src/feedback/templatetags/translate_url.py
@@ -0,0 +1,17 @@
+from django import template
+from django.urls import resolve, reverse
+from django.utils import translation
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def translate_url(context, language):
+    '''
+    used to translate urls for switching languages
+    '''
+    view = resolve(context['request'].path)
+    request_language = translation.get_language()
+    translation.activate(language)
+    url = reverse(view.url_name, args=view.args, kwargs=view.kwargs)
+    translation.activate(request_language)
+    return url
