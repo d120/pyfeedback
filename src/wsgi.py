135957717eeee93cb46bc59068c3bb6ba204a287
4-dash <120916864+4-dash@users.noreply.github.com>
2025-05-20 23:59:35 +0200

    add docker compose and .env

--- src/wsgi.py
+++ src/wsgi.py
@@ -2,7 +2,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+# select in .env which settings to use
+settings_file = os.getenv("SETTINGS_FILE", "settings")
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_file)
 
 application = get_wsgi_application()
